f81a9edcb6ec26bb8f02496d3f545b9808a25de8
Aleksandar Andevski <a.andevski@gmail.com>
2019-05-03 11:28:40 +0200

    Create forum models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Korisnik(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+class Predmet(models.Model):
+	naslov = models.CharField(max_length=200)
+
+class Tema(models.Model):
+	naslov = models.CharField(max_length=200)
+	avtor = models.ForeignKey(Korisnik, on_delete=models.SET_NULL, blank=True, null=True)
+	predmet = models.ForeignKey(Predmet, on_delete=models.CASCADE)
+
+class Poraka(models.Model):
+	tekst = models.TextField()
+	avtor = models.ForeignKey(Korisnik, on_delete=models.SET_NULL, blank=True, null=True)
+	tema = models.ForeignKey(Tema, on_delete=models.CASCADE)
