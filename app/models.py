09cd64e9544581f35f310494d8926d5818ecf9b7
Aleksandar Andevski <a.andevski@gmail.com>
2019-05-08 21:44:56 +0200

    Adapt to PEP8 standards

--- app/models.py
+++ app/models.py
@@ -1,18 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Korisnik(models.Model):
-	user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
 
 class Predmet(models.Model):
-	naslov = models.CharField(max_length=200)
+    naslov = models.CharField(max_length=200)
+
 
 class Tema(models.Model):
-	naslov = models.CharField(max_length=200)
-	avtor = models.ForeignKey(Korisnik, on_delete=models.SET_NULL, blank=True, null=True)
-	predmet = models.ForeignKey(Predmet, on_delete=models.CASCADE)
+    naslov = models.CharField(max_length=200)
+    avtor = models.ForeignKey(Korisnik, on_delete=models.SET_NULL, blank=True, null=True)
+    predmet = models.ForeignKey(Predmet, on_delete=models.CASCADE)
+
 
 class Poraka(models.Model):
-	tekst = models.TextField()
-	avtor = models.ForeignKey(Korisnik, on_delete=models.SET_NULL, blank=True, null=True)
-	tema = models.ForeignKey(Tema, on_delete=models.CASCADE)
+    tekst = models.TextField()
+    avtor = models.ForeignKey(Korisnik, on_delete=models.SET_NULL, blank=True, null=True)
+    tema = models.ForeignKey(Tema, on_delete=models.CASCADE)
