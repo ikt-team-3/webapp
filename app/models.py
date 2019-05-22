dd1d0b3029fc18de617dc5b3a973bb7d455a65ae
Aleksandar Andevski <a.andevski@gmail.com>
2019-05-22 12:01:07 +0200

    Add models to admin

--- app/models.py
+++ app/models.py
@@ -5,9 +5,15 @@ from django.contrib.auth.models import User
 class Korisnik(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
+    class Meta:
+        verbose_name_plural = "korisnici"
+
 
 class Predmet(models.Model):
     naslov = models.CharField(max_length=200)
+
+    class Meta:
+        verbose_name_plural = "predmeti"
 
 
 class Tema(models.Model):
@@ -15,8 +21,14 @@ class Tema(models.Model):
     avtor = models.ForeignKey(Korisnik, on_delete=models.SET_NULL, blank=True, null=True)
     predmet = models.ForeignKey(Predmet, on_delete=models.CASCADE)
 
+    class Meta:
+        verbose_name_plural = "temi"
+
 
 class Poraka(models.Model):
     tekst = models.TextField()
     avtor = models.ForeignKey(Korisnik, on_delete=models.SET_NULL, blank=True, null=True)
     tema = models.ForeignKey(Tema, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = "poraki"
