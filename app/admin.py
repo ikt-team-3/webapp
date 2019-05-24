d6be54a355a6ba2661fbb86bcf49d907869f78ee
Aleksandar Andevski <a.andevski@gmail.com>
2019-05-24 13:53:57 +0200

    Add Korisnik to admin

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Predmet, Tema, Poraka
+from .models import Predmet, Tema, Poraka, Korisnik
 # Register your models here.
 
 admin.site.register(Predmet)
 admin.site.register(Tema)
 admin.site.register(Poraka)
+admin.site.register(Korisnik)
